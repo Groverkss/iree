7f22c63816d4558e02da2f8dceedaef30d202dd5
Groverkss <groverkss@gmail.com>
2023-08-28 22:20:58 +0530

    WIP strategy

--- compiler/src/iree/compiler/Reducer/Strategies/EliminateDispatches.cpp
+++ compiler/src/iree/compiler/Reducer/Strategies/EliminateDispatches.cpp
@@ -4,8 +4,15 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <random>
+
 #include "iree/compiler/Reducer/Strategies/PassDetail.h"
 #include "iree/compiler/Reducer/Strategies/Passes.h"
+
+#include "iree/compiler/Dialect/Flow/IR/FlowOps.h"
+#include "iree/compiler/Dialect/HAL/IR/HALOps.h"
+#include "iree/compiler/Dialect/Util/IR/UtilOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 namespace mlir {
 namespace iree_compiler {
@@ -15,12 +22,39 @@ namespace {
 struct EliminateDispatchesPass
     : public EliminateDispatchesBase<EliminateDispatchesPass> {
   void runOnOperation() override;
+
+  void getRandomTensorInput();
 };
 
 } // namespace
 
 void EliminateDispatchesPass::runOnOperation() {
-  llvm::errs() << "Hello from Eliminate Dispatch Pass\n";
+  Operation *module = getOperation();
+  auto funcOps = module->getRegion(0).getOps<func::FuncOp>();
+
+  if (!llvm::hasSingleElement(funcOps)) {
+    return signalPassFailure();
+  }
+
+  func::FuncOp funcOp = *funcOps.begin();
+
+  // Randomly take a flow.dispatch op and try to replace it.
+  auto dispatchOps = funcOp->getRegion(0).getOps<IREE::Flow::DispatchOp>();
+
+  std::vector<IREE::Flow::DispatchOp> dispatchOpsVec(dispatchOps.begin(),
+                                                     dispatchOps.end());
+
+  while (!dispatchOpsVec.empty()) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, dispatchOpsVec.size() - 1);
+    int index = dis(gen);
+
+    IREE::Flow::DispatchOp dispatchOp = dispatchOpsVec[index];
+    // Replace all outputs with randomly generated tensor.
+    dispatchOp.dump();
+    return;
+  }
 }
 
 std::unique_ptr<Pass> createEliminateDispatchesPass() {
