4b25f10affeb5b60d46f3a184b4b07ad6ae36069
Kunwar Grover <groverkss@gmail.com>
2024-02-15 02:40:48 +0530

    kunwar test

--- compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/TransformExtensions/LLVMGPUExtensions.cpp
@@ -1516,6 +1516,11 @@ transform_dialect::TestAMDGPUContractionDistribution::applyToOne(
     transform::TransformState &state) {
   TestVectorLayoutOptions options(target);
   RewritePatternSet patterns(target.getContext());
+  rewriter.setInsertionPointToStart(&target.getFunctionBody().front());
+  Value laneId =
+      rewriter.create<gpu::ThreadIdOp>(target.getLoc(), gpu::Dimension::x);
+  populateGPUDistributionPatterns(patterns);
+  populateGPUDistributionLayoutAttrPatterns(laneId, patterns);
   populateAMDGPUDistributionPatterns(patterns);
   distributeVectorOps(target, patterns, options);
   return DiagnosedSilenceableFailure::success();
