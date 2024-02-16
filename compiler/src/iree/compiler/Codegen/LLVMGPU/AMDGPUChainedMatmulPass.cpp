4892f287257c9a3d9356fabc6eafe8eca3c44d2f
Kunwar Grover <groverkss@gmail.com>
2024-02-16 20:14:51 +0530

    New chained matmul pass

--- compiler/src/iree/compiler/Codegen/LLVMGPU/AMDGPUChainedMatmulPass.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/AMDGPUChainedMatmulPass.cpp
@@ -13,6 +13,46 @@ namespace mlir::iree_compiler {
 
 namespace {
 
+/// Let's assume that we only have vector.contract with the standard indexing
+/// maps, i.e. for (m, n, k), A: (m, k), B: (k, n), C: (m, n). We will represent
+/// this contract operation by a "@".
+///
+/// Given a matmul:
+///
+/// C = A @ B
+///
+/// This pass decides when to convert this matmul to:
+///
+/// A.T = transpose(A)
+/// B.T = transpose(B)
+/// C.T = A.T @ B.T
+/// C = transpose(C.T)
+///
+/// This is useful when the "@" instruction that the hardware lowers to
+/// has a specific layout (see VectorLayoutInterface for more information)
+/// but the use of C expects a transposed layout to that of that instruction.
+///
+/// For example, for "@" lowering to AMDGPU MFMA instructions, the operands
+/// have layout L and L.T and the result has the layout L.T .
+/// So if you have a chain of matmuls:
+///
+/// C (L.T) = A (L) @ B (L.T)
+/// E (L.T) = C (L.T)  @ D (L.T)
+///            ^^^^^^^
+///            Expected layout by instruction is L
+///
+/// To fix this, we can apply this transpose on the first matrix:
+///
+/// C.T (L.T) = A.T (L) @ B (L.T)
+/// C   (L)   = transpose C.T (L.T)
+/// E   (L.T) = C (L)  @ D (L.T)
+///            ^^^^^
+///            Layout matches the instruction!
+///
+/// Note that the mathematical transformation
+/// C = A @ B --> C.T = B.T @ A.T
+/// is only defined on standard "@" function, it cannot be just used on
+/// a function like matmul_transpose_b.
 struct AMDGPUPrepareForChainedMatmulPass
     : public AMDGPUPrepareForChainedMatmulBase<
           AMDGPUPrepareForChainedMatmulPass> {
@@ -31,37 +71,42 @@ struct AMDGPUPrepareForChainedMatmulPass
     Value rhs = contractOp.getRhs();
     Value acc = contractOp.getAcc();
     rewriter.setInsertionPoint(contractOp);
-    Value transposed = rewriter.create<vector::TransposeOp>(
-        contractOp.getLoc(), acc, SmallVector<int64_t>{1, 0});
-    AffineExpr m, n, k;
-    bindDims(rewriter.getContext(), m, n, k);
-    using MapList = ArrayRef<ArrayRef<AffineExpr>>;
-    auto infer = [](MapList m) { return AffineMap::inferFromExprList(m); };
-    SmallVector<AffineMap> newIndexingMaps = infer({{n, k}, {m, k}, {n, m}});
+    acc = rewriter.create<vector::TransposeOp>(contractOp.getLoc(), acc,
+                                               SmallVector<int64_t>{1, 0});
+
+    if (!isOperandSwapInvariant(contractOp)) {
+      lhs = rewriter.create<vector::TransposeOp>(contractOp.getLoc(), lhs,
+                                                 SmallVector<int64_t>{1, 0});
+      rhs = rewriter.create<vector::TransposeOp>(contractOp.getLoc(), rhs,
+                                                 SmallVector<int64_t>{1, 0});
+    }
+
     vector::ContractionOp swappedOp = rewriter.create<vector::ContractionOp>(
-        contractOp.getLoc(), rhs, lhs, transposed,
-        rewriter.getAffineMapArrayAttr(newIndexingMaps),
+        contractOp.getLoc(), rhs, lhs, acc, contractOp.getIndexingMaps(),
         contractOp.getIteratorTypesAttr());
-    Value newResult = swappedOp.getResult();
-    transposed = rewriter.create<vector::TransposeOp>(
-        contractOp.getLoc(), newResult, SmallVector<int64_t>{1, 0});
-    rewriter.replaceOp(contractOp, transposed);
+    rewriter.replaceOpWithNewOp<vector::TransposeOp>(
+        contractOp, swappedOp.getResult(), SmallVector<int64_t>{1, 0});
   }
 
-  /// The only compatible indexing map corresponds to
-  /// the matmul_transpose_b, and is
-  /// (m, n, k) -> (m, k)
-  /// (m, n, k) -> (n, k)
-  /// (m, n, k) -> (m, n)
+  /// For a matmul_transpose_b, this transformation boils down to an operand
+  /// swap and result transpose:
   ///
-  /// We only check for matmul_transpose_b because transposing the inputs of
-  /// the matmul for it still produces a matmul_transpose_b, which is good
-  /// for reads. We could probably do this for other matmuls also, but
-  /// we need to think if we want to enable that.
-  bool isCompatibleIndexingMap(vector::ContractionOp contractOp,
-                               MLIRContext *ctx) const {
+  /// def matmul_transpose_b(A, B):
+  ///   B.T = transpose(B)
+  ///   C = A @ B.T
+  ///   return C
+  ///
+  /// def matmul_transpose_b_swapped(A, B):
+  ///   A.T = transpose(A)
+  ///   C.T = B @ A.T
+  ///   C   = transpose(C.T)
+  ///   return C
+  ///
+  /// TODO: This check applies more generally when one of the operands in the
+  /// function is transposed compared to what "@" expects.
+  bool isOperandSwapInvariant(vector::ContractionOp contractOp) const {
     AffineExpr m, n, k;
-    bindDims(ctx, m, n, k);
+    bindDims(contractOp.getContext(), m, n, k);
     using MapList = ArrayRef<ArrayRef<AffineExpr>>;
     auto infer = [](MapList m) { return AffineMap::inferFromExprList(m); };
     SmallVector<AffineMap> newIndexingMaps = infer({{m, k}, {n, k}, {m, n}});
@@ -84,7 +129,7 @@ struct AMDGPUPrepareForChainedMatmulPass
       }
 
       // Check if the chainParent is a compatible matmul.
-      if (isCompatibleIndexingMap(chainParent, candidate.getContext())) {
+      if (isOperandSwapInvariant(chainParent)) {
         return chainParent;
       }
     }
@@ -107,6 +152,9 @@ struct AMDGPUPrepareForChainedMatmulPass
       }
       auto chainParent = maybeChainedParent.value();
       swapOperandsAndTranspose(rewriter, chainParent);
+
+      // TODO: Only swap the candidate in case of interference.
+      swapOperandsAndTranspose(rewriter, candidate);
     }
   }
 };
